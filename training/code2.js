cc267328a80da403e52ca00838dd68c354cdc3be
Vadim_Shchiglo <papuccio77@mail.ru>
2023-11-07 19:19:54 +0300

    third

---
+++ training/code2.js
@@ -0,0 +1,92 @@
+// let name = 5;
+// let myLife;
+// myLife = 'freelancer';
+// console.log(myLife);
+
+
+
+// let myAge = 36;
+// let myNewAge = 18;
+// myAge = myNewAge;
+// console.log(myAge);
+
+
+
+
+// for (let i = 2; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+
+// const userProfile = {
+//     name: 'Freelancer',
+//     age: 20,
+//     message: 'I love you'
+// }
+// console.log(userProfile);
+// userProfile.age = 24;
+// console.log(userProfile);
+
+
+
+
+
+// --------------------------------------------------------------
+//КЛЮЧЕВОЕ СЛОВО VAR
+
+//Области видимости
+
+/*
+//Использование переменной до её объявления
+
+"use strict"
+
+age = 35;
+console.log(age);
+
+var age;
+
+*/
+
+
+/*
+function testBlockLet() {
+	let myAgeLet = 36;
+}
+testBlockLet();
+
+//Переменная НЕ видна за пределами блока
+//console.log(myAgeLet);
+
+function testBlockVar() {
+	var myAgeVar = 36;
+}
+testBlockVar();
+
+//Переменная НЕ видна за пределами блока
+console.log(myAgeVar);
+*/
+
+/*
+if (true) {
+	let sizeLet = 50;
+}
+//Переменная НЕ видна за пределами блока
+//console.log(sizeLet);
+
+if (true) {
+	var sizeVar = 50;
+}
+//Переменная ВИДНА за пределами блока
+console.log(sizeVar);
+*/
+
+// --------------------------------------------------------------
+
+
+// let colorEyes;
+// let user;
+// let userName = 'Вася';
+// user = userName;
+// console.log(user);
